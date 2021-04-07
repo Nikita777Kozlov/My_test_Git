f5637710e4b4d7adacf33b8a543ed9352a9c0cf3
Nikita_Kozlov <mister.nikita2000@bk.ru>
2021-04-07 12:15:14 +0300

    Add + Second println

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Hello_world {
 
     public static void main(String[] args) {
         System.out.println("Hello Yesenyasha,kotoraya grizit krashky ot bytilki");
+        System.out.println("Hello Nikita bezdelnik");
     }
 
 }
